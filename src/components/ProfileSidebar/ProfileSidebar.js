7d36a3fa5b8ece43621a2478123151686fcdbe35
Vladislav Kozello <vkozello2005@gmail.com>
2024-10-14 11:01:32 +0300

    add icons to sidebar

--- src/components/ProfileSidebar/ProfileSidebar.js
+++ src/components/ProfileSidebar/ProfileSidebar.js
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { BsFillPersonFill } from "react-icons/bs";
 import { IoClose } from "react-icons/io5";
+import { IoExitOutline } from "react-icons/io5";
+import { IoAlbumsOutline } from "react-icons/io5";
+import { IoBuildOutline } from "react-icons/io5";
+import { IoPersonCircleOutline } from "react-icons/io5";
 import './ProfileSidebar.css'
 
 const ProfileSidebar = () => {
@@ -32,10 +36,10 @@ const ProfileSidebar = () => {
       <div className={`p-sidebar ${isOpen ? 'open' : ''}`}>
         <IoClose onClick={toggleSidebar} className="close-butt"/>
         <ul>
-          <li><button onClick={goToProfile}>Мой профиль</button></li>
-          <li><button onClick={goToRepos}>Мои репозитории</button></li>
-          <li><button >Настройки</button></li>
-          <li><button >Выйти</button></li>
+          <li><button onClick={goToProfile}><IoPersonCircleOutline className='icon'/>Мой профиль</button></li>
+          <li><button onClick={goToRepos}><IoAlbumsOutline className="icon"/>Мои репозитории</button></li>
+          <li><button ><IoBuildOutline className="icon"/>Настройки</button></li>
+          <li><button ><IoExitOutline className="icon"/>Выйти</button></li>
         </ul>
       </div>
     </div>
